e357c47cc16a17784859e9d31aab0b67a4463287
kaherro <torkomyanvv@gmail.com>
2026-07-10 21:30:04 +0300

    [feat]: Added expirations and getters in LRUCache.cpp

--- include/LRUCache.h
+++ include/LRUCache.h
@@ -11,11 +11,12 @@ private:
     std::list<KEY> order;
     HashMap<KEY, VAL> values;
     HashMap<KEY, typename std::list<KEY>::iterator> iterators;
+    HashMap<KEY, long long> expirations; 
 
 public:
-    LRUCache(size_t capacity) : capacity(capacity), values(capacity * 2), iterators(capacity * 2) {}
+    LRUCache(size_t capacity) : capacity(capacity), values(capacity * 2), iterators(capacity * 2), expirations(capacity * 2) {}
 
-    std::optional<VAL> get(const KEY& key) {
+    std::optional<VAL> get(const KEY &key) {
         auto val = values.get(key);
         if (!val) {
             auto it = iterators.get(key);
@@ -32,7 +33,15 @@ public:
         return val;
     }
 
-    void set(const KEY& key, const VAL& value, int ttl = 0) {
+    void set(const KEY &key, const VAL &value, int ttl = 0) {
+        long long expires_at = 0;
+        if(ttl > 0) {
+            expires_at = std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::system_clock::now().time_since_epoch()
+            ).count() + ttl;
+        }
+        expirations.set(key, expires_at); 
+
         if (values.get(key)) {
             values.set(key, value, ttl);
             auto it = iterators.get(key);
@@ -52,12 +61,29 @@ public:
         iterators.set(key, order.begin());
     }
 
-    bool del(const KEY& key) {
+    bool del(const KEY &key) {
         if (!values.get(key)) return false;
         auto it = iterators.get(key);
         order.erase(*it);
         values.del(key);
         iterators.del(key);
+        expirations.del(key); 
         return true;
+    }
+
+    std::vector<KEY> keys() const {
+        std::vector<KEY> res; 
+        for(const auto &key : order) {
+            res.push_back(key); 
+        }
+        return res; 
+    }
+
+    std::optional<VAL> get_val_without_update(const KEY &key) {
+        return values.get(key);
+    }
+
+    std::optional<long long> get_expiration_without_update(const KEY &key) {
+        return expirations.get(key);
     }
 };
